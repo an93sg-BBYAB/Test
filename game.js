2de4c6022e5d97e6c49e97e3449c8c986ffbb4d8
an93sg-BBYAB <an93sg@gmail.com>
2025-10-27 23:43:25 +0900

    Update game.js
    Update Jump Cooldown time from 0.2 sec to 1.0 sec

--- game.js
+++ game.js
@@ -60,7 +60,7 @@ const player = {
     
     // (★ 추가!) 점프 쿨타임 변수
     isJumpCoolingDown: false,      // 현재 쿨타임 중인가?
-    jumpCooldown: 0.2,           // 쿨타임 시간 (0.2초)
+    jumpCooldown: 1.0,           // 쿨타임 시간 (0.2초)
     jumpCooldownStartTime: 0,    // 쿨타임 시작 시간
     
     // (★ 추가!) 8방향 스프라이트 변수
@@ -217,3 +217,4 @@ function animate() {
     update();
     renderer.render(scene, camera);
 }
+
